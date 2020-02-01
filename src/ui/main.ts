9511ace65e2c0447bb8dc92bc8c10877ffcf5549
sine <963366202@qq.com>
2020-02-01 15:12:08 +0800

    add macro highlight in monaco

--- src/ui/main.ts
+++ src/ui/main.ts
@@ -4,9 +4,15 @@
  */
 import '../kiwi/ui/css/global.css'
 import 'highlight.js/styles/solarized-light.css'
+import { language as mdLang } from 'monaco-editor/esm/vs/basic-languages/markdown/markdown'
 import manager from './ItemManager'
 
 async function run() {
+  mdLang.tokenizer.root.unshift([/\{\{/, { token: 'keyword', bracket: '@open', next: '@macroblock', nextEmbedded: 'javascript' }])
+  mdLang.tokenizer.macroblock = [
+    [/\}\}/, { token: 'keyword', bracket: '@close', next: '@pop', nextEmbedded: '@pop' }],
+    [/[\s\S]*?/, 'variable.source'],
+  ]
   await manager.init()
 }
 
