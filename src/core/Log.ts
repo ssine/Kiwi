59aa9caeb9a55e0b45a3b6737ede446b1c2a752a
sine <963366202@qq.com>
2020-02-10 13:30:04 +0800

    rename

---
+++ src/core/Log.ts
@@ -0,0 +1,28 @@
+import { createLogger, format, transports } from 'winston'
+import * as logform from 'logform'
+
+let loggingLevel: string = 'debug'
+
+function initLogger(level: string) {
+  loggingLevel = level
+}
+
+function getLogger(moduleName: string) {
+  const logger = createLogger({
+    level: loggingLevel,
+    format: format.combine(
+      format.printf((info: logform.TransformableInfo): string => {
+        return `[${info.level}] ${moduleName}: ${info.message}`
+      })
+    ),
+    transports: [
+      new transports.Console()
+    ]
+  })
+  return logger
+}
+
+export {
+  initLogger,
+  getLogger
+}
