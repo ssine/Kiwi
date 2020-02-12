bf2b43abd9bf052fdb9ec9051895349c9c221114
sine <963366202@qq.com>
2020-02-13 01:07:30 +0800

    pass options to graphviz

--- src/lib/plugin/GraphvizPlugin.ts
+++ src/lib/plugin/GraphvizPlugin.ts
@@ -11,9 +11,10 @@ export default class GraphvizPlugin extends RenderPlugin {
     return ['graphviz']
   }
   getFunctionForItem() {
-    return async (input: string): Promise<string> => {
+    return async (input: string, config: any): Promise<string> => {
       return new Promise((res, rej) => {
-        this.viz.renderString(input)
+        let cfg = config ? config : {}
+        this.viz.renderString(input, cfg)
           .then((v: string) => res(`<div>${v}</div>`))
           .catch((err:string) => {
             this.viz = new Viz({ Module, render });
