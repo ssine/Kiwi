510e9730b98886064ef0f62f26ec7cc92cf96bdd
sine <963366202@qq.com>
2020-02-16 12:30:08 +0800

    add svg plugin

---
+++ src/lib/plugin/SVGPlugin.ts
@@ -0,0 +1,22 @@
+import { RenderPlugin } from '../../core/Plugin'
+
+export default class SVGPlugin extends RenderPlugin {
+  init() {
+  }
+
+  getNames() {
+    return ['svg']
+  }
+
+  getDescription() {
+    return `give source url and a optional width of a svg, return the html to embed it.`
+  }
+
+  getFunctionForItem() {
+    return async (src: string, width?: string | number): Promise<string> => {
+      if (typeof width === 'string' && width[width.length - 1] === '%')
+        return `<center><embed src="${src}" style="width:${width};max-width:100%;" type="image/svg+xml" /></center>`
+      return `<center><embed src="${src}" style="width:${width}px;max-width:100%;" type="image/svg+xml" /></center>`
+    }
+  }
+}
