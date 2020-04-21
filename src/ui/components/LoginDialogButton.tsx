94b3edcbd0dce45cb60eec4a1cb858e4fd4ca957
sine <963366202@qq.com>
2020-04-21 13:36:50 +0800

    fix login cookie time

--- src/ui/components/LoginDialogButton.tsx
+++ src/ui/components/LoginDialogButton.tsx
@@ -50,7 +50,7 @@ export default class LoginDialogButton extends React.Component<{}, LoginDialogBu
       password: (document.getElementById('kiwi-login-password') as HTMLInputElement).value
     })
     if (res.success) {
-      setCookie('token', res.token, 365*24*60)
+      setCookie('token', res.token, 365*24*3600)
       window.location.reload()
     } else {
       document.getElementById('kiwi-login-error-msg').innerText = res.reason
