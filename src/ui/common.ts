10eed78d2e42f6ce71af3399a6bee7656be4b071
sine <963366202@qq.com>
2020-05-04 16:38:11 +0800

    add color processing utils

--- src/ui/common.ts
+++ src/ui/common.ts
@@ -33,7 +33,7 @@ function isLinkInternal(el: HTMLAnchorElement): boolean {
 /**
  * Get the postition of an element relative to the root document
  */
-function getPositionToDocument(el: Element): {top: number, left: number} {
+function getPositionToDocument(el: Element): { top: number, left: number } {
   const rect = el.getBoundingClientRect()
   const scrollLeft = window.pageXOffset || document.documentElement.scrollLeft
   const scrollTop = window.pageYOffset || document.documentElement.scrollTop
@@ -62,11 +62,110 @@ function getCookie(key: string) {
   return ''
 }
 
+const CSSColorToRGBA = (function () {
+  var canvas = document.createElement('canvas')
+  canvas.width = canvas.height = 1
+  var ctx = canvas.getContext('2d')
+
+  return function (col: string) {
+    ctx.clearRect(0, 0, 1, 1)
+    // In order to detect invalid values,
+    // we can't rely on col being in the same format as what fillStyle is computed as,
+    // but we can ask it to implicitly compute a normalized value twice and compare.
+    ctx.fillStyle = '#000'
+    ctx.fillStyle = col
+    var computed = ctx.fillStyle
+    ctx.fillStyle = '#fff'
+    ctx.fillStyle = col
+    if (computed !== ctx.fillStyle) {
+      return {r: 0, g: 0, b: 0, a: 0}// invalid color
+    }
+    ctx.fillRect(0, 0, 1, 1)
+    let data = ctx.getImageData(0, 0, 1, 1).data
+    return {
+      r: data[0],
+      g: data[1],
+      b: data[2],
+      a: data[3],
+    }
+  }
+})()
+
+function RGBtoHSV(color: {r: number, g: number, b: number}) {
+  let r = color.r
+  let g = color.g
+  let b = color.b
+  var max = Math.max(r, g, b), min = Math.min(r, g, b),
+    d = max - min,
+    h,
+    s = (max === 0 ? 0 : d / max),
+    v = max / 255;
+
+  switch (max) {
+    case min: h = 0; break;
+    case r: h = (g - b) + d * (g < b ? 6 : 0); h /= 6 * d; break;
+    case g: h = (b - r) + d * 2; h /= 6 * d; break;
+    case b: h = (r - g) + d * 4; h /= 6 * d; break;
+  }
+
+  return {
+    h: h,
+    s: s,
+    v: v
+  };
+}
+
+function HSVtoRGB(color: {h: number, s: number, v: number}) {
+  let h = color.h
+  let s = color.s
+  let v = color.v
+  let i = Math.floor(h * 6);
+  let f = h * 6 - i;
+  let p = v * (1 - s);
+  let q = v * (1 - f * s);
+  let t = v * (1 - (1 - f) * s);
+  let r: number
+  let b: number
+  let g: number
+  switch (i % 6) {
+    case 0: r = v, g = t, b = p; break;
+    case 1: r = q, g = v, b = p; break;
+    case 2: r = p, g = v, b = t; break;
+    case 3: r = p, g = q, b = v; break;
+    case 4: r = t, g = p, b = v; break;
+    case 5: r = v, g = p, b = q; break;
+  }
+  return {
+    r: Math.round(r * 255),
+    g: Math.round(g * 255),
+    b: Math.round(b * 255)
+  };
+}
+
+function RGBtoCSSColor(rgb: {r: number, g: number, b: number}): string {
+  return `rgb(${rgb.r}, ${rgb.g}, ${rgb.b})`
+}
+
+function setPageColors(hue: number) {
+  let rootStyle = (document.querySelector(':root') as HTMLElement).style
+  rootStyle.setProperty('--primaryColor', RGBtoCSSColor(HSVtoRGB({h: hue, s: 1, v: 1})))
+  rootStyle.setProperty('--lineColor', RGBtoCSSColor(HSVtoRGB({h: hue, s: 0.54, v: 0.62})))
+  rootStyle.setProperty('--blockColor', RGBtoCSSColor(HSVtoRGB({h: hue, s: 0.26, v: 0.84})))
+  rootStyle.setProperty('--blockColorLight', RGBtoCSSColor(HSVtoRGB({h: hue, s: 0.24, v: 0.90})))
+  rootStyle.setProperty('--blockColorLighter', RGBtoCSSColor(HSVtoRGB({h: hue, s: 0.17, v: 0.93})))
+  rootStyle.setProperty('--areaColor', RGBtoCSSColor(HSVtoRGB({h: hue, s: 0.04, v: 0.98})))
+}
+
 export {
   postJSON,
   postFile,
   isLinkInternal,
   getPositionToDocument,
   setCookie,
-  getCookie
+  getCookie,
+  CSSColorToRGBA,
+  RGBtoHSV,
+  HSVtoRGB,
+  RGBtoCSSColor,
+  setPageColors,
 }
