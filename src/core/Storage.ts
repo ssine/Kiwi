fe13f844b35f8f016b5aa1bc9639f39f3e7599a3
ssine <liu.siyao@qq.com>
2021-04-26 03:39:26 +0800

    add storage provider interface

--- src/core/Storage.ts
+++ src/core/Storage.ts
@@ -2,18 +2,21 @@
  * Storage modules provide a two way mapping between an item and its serialized representation.
  */
 import { Readable } from 'stream'
+import { BaseItem } from './BaseItem'
+import { MIME } from './Common'
 
 type Node = {
-  meta: Record<string, any>
-  content: string
-  getReadStream(): Readable
+  type: MIME // MIME type of node content
+  meta: Record<string, any> // a kv storage associated with node
+  content?: string // text content of non-binary node
+  getReadStream?: () => Readable // function to get readable stream of binary node
 }
 
 interface StorageProvider {
-  /**
-   * aaa
-   */
-  a(): void
+  getItem: (uri: string) => Promise<BaseItem | null>
+  putItem: (uri: string, item: BaseItem) => Promise<void>
+  deleteItem: (uri: string) => Promise<void>
+  getAllItems: () => Promise<Record<string, BaseItem>>
 }
 
 export { Node, StorageProvider }
