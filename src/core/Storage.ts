283fb79a07e6abfe248cac9aeb02cf7c9d27702a
ssine <liu.siyao@qq.com>
2021-04-26 02:10:58 +0800

    add new storage and gts

---
+++ src/core/Storage.ts
@@ -0,0 +1,22 @@
+/**
+ * Storage modules provide a two way mapping between an item and its serialized representation.
+ */
+import { Readable } from "stream"
+
+type Node = {
+  meta: Record<string, any>
+  content: string
+  getReadStream(): Readable
+}
+
+interface StorageProvider {
+  /**
+   * aaa
+   */
+  a(): void
+}
+
+export {
+  Node,
+  StorageProvider
+}
