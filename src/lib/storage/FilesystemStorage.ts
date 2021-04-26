dcb5e82f05d4e23441e1f5098ab6e92a40a0d6e8
ssine <liu.siyao@qq.com>
2021-04-27 01:28:37 +0800

    half work

--- src/lib/storage/FilesystemStorage.ts
+++ src/lib/storage/FilesystemStorage.ts
@@ -1,4 +1,5 @@
 import { BaseItem } from '../../core/BaseItem'
+import { getExtensionFromMIME, MIME, renderableMIME } from '../../core/Common'
 import { StorageProvider } from '../../core/Storage'
 
 class FilesystemStorage implements StorageProvider {
@@ -8,7 +9,28 @@ class FilesystemStorage implements StorageProvider {
   }
 }
 
-const uriToPath = (uri: string): string => {
+/**
+ * Construct a one-one mapping between uri and file path.
+ * URI structures are inherently tree shaped, however folders cannot hold contents, thus all the
+ * non-leaf nodes are mapped to index.md in that folder, and the original [index] uri are mapped
+ * to __index__.md, and so on.
+ * @param uri a normalized uri
+ * @param type mime type of content
+ * @returns file path to save content in
+ */
+const uriToPath = (uri: string, type: MIME): string => {
+  let path = uri
+  if (renderableMIME.has(type)) {
+    path += `.${getExtensionFromMIME(type)}`
+  }
+  return uri
+}
+
+const pathToUri = (path: string): string => {
+  let path = uri
+  if (renderableMIME.has(type)) {
+    path += `.${getExtensionFromMIME(type)}`
+  }
   return uri
 }
 
