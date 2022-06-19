9b5b5cdda991d400ba3f42f8021e95cc46539002
sine <liu.siyao@qq.com>
2022-06-20 03:08:06 +0800

    feat: auto list on item empty

--- src/core/render.ts
+++ src/core/render.ts
@@ -2,9 +2,13 @@ import { parse } from './Parser'
 import { ItemContext, processRenderPlugin } from './plugin'
 import { ServerItem } from './ServerItem'
 
+const defaultContent = `{{list(all => all.filter(i => i[0].startsWith(kiwi.uri) && (i[0].slice(kiwi.uri.length).match(/\\//g) || []).length === 1))}}`
+
 export const renderItem = async (uri: string, item: ServerItem): Promise<void> => {
+  const content = item.content || ''
+  const input = content.trim().length === 0 && !item.header.noPlaceholderList ? defaultContent : content
   item.renderedHTML = parse({
-    input: await processRenderPlugin(uri, item.content || '', new ItemContext(uri)),
+    input: await processRenderPlugin(uri, input, new ItemContext(uri)),
     uri: uri,
     type: item.type,
   })
