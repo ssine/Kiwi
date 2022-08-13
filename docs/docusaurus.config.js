b9cd0f3811078a598f993c2a64ff8a4374365b31
sine <liu.siyao@qq.com>
2022-08-14 05:16:58 +0800

    feat: tmp baseurl before custom kiwi domain

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -9,7 +9,7 @@ const config = {
   title: 'Kiwi',
   tagline: 'A Flat File Personal Wiki',
   url: 'https://your-docusaurus-test-site.com',
-  baseUrl: '/',
+  baseUrl: '/Kiwi/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
