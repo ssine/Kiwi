f3e84cfb39bec8887abedf3cac3b312b297510ed
sine <liu.siyao@qq.com>
2022-08-14 05:10:51 +0800

    feat: add github action

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -105,7 +105,7 @@ const config = {
             items: [
               {
                 label: 'Tutorial',
-                to: '/docs/intro',
+                to: '/docs/tutorial/index',
               },
             ],
           },
