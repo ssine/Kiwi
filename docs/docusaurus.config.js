aa952eed566ee6639f39f1afd1a58ee272a4af9a
sine <liu.siyao@qq.com>
2022-08-14 19:13:43 +0800

    feat: update baseurl

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -9,7 +9,7 @@ const config = {
   title: 'Kiwi',
   tagline: 'A Flat File Personal Wiki',
   url: 'https://your-docusaurus-test-site.com',
-  baseUrl: '/Kiwi/',
+  baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
